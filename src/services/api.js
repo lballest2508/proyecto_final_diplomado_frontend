8c8e7defc5164caa82b209c33bc3ce3aaa4a0e64
Lballest25 <lballest@cuc.edu.co>
2025-04-20 17:39:37 -0500

    Agregar archivo .env y actualizar la URL base de la API en api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${process.env.VITE_API}/api`,
 });
 
 export const getProducts = async () => {
